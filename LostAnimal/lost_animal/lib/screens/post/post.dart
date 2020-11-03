80c2dbb16d6dacf9b6f36e20a4adc056bcfe4886
Eline <elineschepers1999@gmail.com>
2020-11-03 18:08:40 +0100

    added some changes

--- LostAnimal/lost_animal/lib/screens/post/post.dart
+++ LostAnimal/lost_animal/lib/screens/post/post.dart
@@ -22,6 +22,7 @@ class _PostState extends State<Post> {
   User user;
   _PostState({this.user});
 
+  String error = "";
   //textfield state
   String name = '';
   String animalType = '';
@@ -36,6 +37,7 @@ class _PostState extends State<Post> {
     return loading
         ? Loading()
         : Scaffold(
+            resizeToAvoidBottomInset: false,
             backgroundColor: Colors.brown[100],
             appBar: AppBar(
               backgroundColor: Colors.brown[400],
@@ -50,97 +52,110 @@ class _PostState extends State<Post> {
                     label: Text('Home'))
               ],
             ),
-            body: Container(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'name'),
-                        validator: (val) => val.isEmpty
-                            ? 'Enter name of the lost animal'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => name = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                            hintText: 'animaltype'),
-                        validator: (val) => val.length < 6
-                            ? 'give the type of your animal'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => animalType = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                            hintText: 'description'),
-                        validator: (val) => val.length < 6
-                            ? 'give a description of the lost animal'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => beschrijving = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'street'),
-                        validator: (val) => val.length < 6
-                            ? 'give the streetname of the spot where the animal has been seen last'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => straatnaam = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                            hintText: 'house number'),
-                        validator: (val) =>
-                            val.length < 6 ? 'give the house number' : null,
-                        onChanged: (val) {
-                          setState(() => huisnummer = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'city'),
-                        validator: (val) =>
-                            val.length < 6 ? 'give the city' : null,
-                        onChanged: (val) {
-                          setState(() => woonplaats = val);
-                        }),
-                    SizedBox(height: 20.0),
-                    RaisedButton(
-                      color: Colors.pink[400],
-                      child: Text(
-                        'Post',
-                        style: TextStyle(color: Colors.white),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'name'),
+                          validator: (val) => val.isEmpty
+                              ? 'Enter name of the lost animal'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => name = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'animaltype'),
+                          validator: (val) => val.length < 6
+                              ? 'give the type of your animal'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => animalType = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'description'),
+                          validator: (val) => val.length < 6
+                              ? 'give a description of the lost animal'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => beschrijving = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'street'),
+                          validator: (val) => val.length < 6
+                              ? 'give the streetname of the spot where the animal has been seen last'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => straatnaam = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'house number'),
+                          validator: (val) =>
+                              val.length < 6 ? 'give the house number' : null,
+                          onChanged: (val) {
+                            setState(() => huisnummer = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'city'),
+                          validator: (val) =>
+                              val.length < 6 ? 'give the city' : null,
+                          onChanged: (val) {
+                            setState(() => woonplaats = val);
+                          }),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        color: Colors.pink[400],
+                        child: Text(
+                          'Post',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _db.updateAnimalData(
+                                name,
+                                beschrijving,
+                                animalType,
+                                straatnaam,
+                                huisnummer,
+                                woonplaats,
+                                user.getUid());
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                    'wrong credentials or animal might already exists';
+                                loading = false;
+                              });
+                            } else {
+                              widget.toggleView();
+                            }
+                          }
+                        },
                       ),
-                      onPressed: () async {
-                        await _db.updateAnimalData(
-                            name,
-                            beschrijving,
-                            animalType,
-                            straatnaam,
-                            huisnummer,
-                            woonplaats,
-                            user.getUid());
-                      },
-                    ),
-                    SizedBox(height: 12.0),
-                    //Text(
-                    //error,
-                    //style: TextStyle(color: Colors.red, fontSize: 14.0),
-                    //)
-                  ],
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
+            ));
   }
 }
--- LostAnimal/lost_animal/lib/services/database.dart
+++ LostAnimal/lost_animal/lib/services/database.dart
@@ -16,22 +16,30 @@ class DatabaseService {
 
   Future updateAnimalData(String name, String beschrijving, String animalType,
       String straatnaam, String huisnr, String gemeente, String userid) async {
-    return await lostAnimalCollection.document().setData({
-      'name': name,
-      'beschrijving': beschrijving,
-      'animalType': animalType,
-      'straatnaam': straatnaam,
-      'huisnr': huisnr,
-      'gemeente': gemeente,
-      'userid': userid
-    });
+    print(lostAnimalCollection.toString());
+    try {
+      return await lostAnimalCollection
+          .document(userid + name + animalType)
+          .setData({
+        'name': name,
+        'beschrijving': beschrijving,
+        'animalType': animalType,
+        'straatnaam': straatnaam,
+        'huisnr': huisnr,
+        'gemeente': gemeente,
+        'userid': userid
+      });
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 
   // animal list from snapshot
   List<Animal> _animalListFromSnapshot(QuerySnapshot snapshot) {
     return snapshot.documents.map((doc) {
       return Animal(
-          beschrijving: doc.data['beschrijving'] ?? '',
+          animalType: doc.data['animalType'] ?? '',
           name: doc.data['name'] ?? '');
     }).toList();
   }
